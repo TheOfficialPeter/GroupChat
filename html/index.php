094fbfb7562fc4055e7b958ab0bd15118eb71463
Peter <57006688+TheOfficialPeter@users.noreply.github.com>
2022-12-04 22:25:09 +0000

    Initial update: adding first files

---
+++ html/index.php
@@ -0,0 +1,39 @@
+<html>
+<head>
+</head>
+<link href="../css/main.css" rel="stylesheet"/>
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Fanwood+Text&family=Inter&family=Montserrat&family=Open+Sans:wght@300&family=Roboto:wght@300&display=swap" rel="stylesheet">
+<body>
+    <div id="nav">
+        <h1 id="nav-title">
+            Pieter's group chat app
+        </h1>
+    </div>
+    <h1 id="page-title">
+        Group chat code:
+    </h1>
+    <div id="pin-box">
+        <div id="pin-input">
+            <span id="pin-input-text">0</span>
+        </div>
+        <div id="pin-input">
+            <span id="pin-input-text">0</span>
+        </div>
+        <div id="pin-input">
+            <span id="pin-input-text">0</span>
+        </div>
+        <div id="pin-input">
+            <span id="pin-input-text">0</span>
+        </div>
+        <div id="pin-input">
+            <span id="pin-input-text">0</span>
+        </div>
+    </div>
+    <button id="joinBtn" type="submit">
+        <span id="joinBtnText">Join</span>
+    </button>
+    <script src="../js/main.js"></script>
+</body>
+</html>
